971d8d5e27e6a9693ef6a64ac223892c140bc705
Vivek Kumar <Kumarvivek79510@gmail.com>
2022-05-06 09:16:30 +0530

    Initial Upload

--- game.js
+++ game.js
@@ -1,5 +1,11 @@
 var gamePattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
+var userClickedPattern = [];
+
+$("button").click(function(){
+  var userChosenColour = $(this).att("id");
+  userClickedPattern.push(userChosenColour);
+});
 
 function nextSequence() {
   var randomNumber = Math.floor(Math.random()*4);
@@ -8,4 +14,15 @@ function nextSequence() {
   $("#"+randomChoosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
   var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
   audio.play();
+}
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour){
+  $("#"+currentColor").addClass("pressed");
+  setTimeout(function(){
+    $("#"+currentColour).removeClass("pressed");
+  },100);
 }
